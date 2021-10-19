747c59d25d4f2f9c62c22f5a1683c469f26b4634
Vitor Bonfim <50812235+vitorbnfm@users.noreply.github.com>
2021-10-19 18:10:23 -0300

    removed json file

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const puppeteer = require('puppeteer');
 const readlineSync = require('readline-sync');
-const fs = require('fs');
+//const fs = require('fs');
 var Excel = require("exceljs");
 
 console.log('Web Scraping Google Ads');
-
 
 async function scraper() {
   const browser = await puppeteer.launch({ headless: true });
@@ -30,23 +29,39 @@ async function scraper() {
     return links;
   });
 
-  const filename = './scraperResult.xlsx';
+  const filename = 'C:/Users/Vitor Bonfim/OneDrive/Documentos/scraperResult.xlsx';
   const workbook = new Excel.Workbook();
   await workbook.xlsx.readFile(filename);
 
-  const  worksheet = workbook.getWorksheet("resultado");
-  let row = worksheet.getRow(1);
-  
-  row.getCell(1).value = results[0].link;
+  const worksheet = workbook.getWorksheet("resultado");
+
+  worksheet.columns = [
+    { header: 'Link', key: 'link', width: 80},
+    { header: 'Anúncio', key: 'anuncio', width: 70 },
+    { header: 'Data', key: 'data', width: 40 },
+  ];  
+
+  for (let i = 0; i < results.length; i++) {
+    worksheet.addRow({
+      link: results[i].link ,
+      anuncio: results[i].anuncio,
+      data: results[i].data,
+     })
+  }
+
 
   workbook.xlsx.writeFile(filename);
-
  
-  fs.writeFile('./ads.json', JSON.stringify(results, null, 2), err => err ? console.log(err): null);
-  
-  
-  //console.log(JSON.stringify(results))
+  //fs.writeFile('./ads.json', JSON.stringify(results, null, 2), err => err ? console.log(err): null);
+    
+  console.log(JSON.stringify(results))
   await browser.close();
 }
 
 scraper();
+
+
+
+
+
+
